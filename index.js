a38e9f6c67b408cf41fb27a93c1b270f918fa0c0
Haodong Luo <42688847+LM233@users.noreply.github.com>
2022-09-29 19:34:07 -0400

    Commit to the npm server.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import "./standalone"
-function socratProjector(dataStr){
+function socratProjector(height, width, dataStr){
     const projector = document.createElement('vz-projector-app')
     projector.setAttribute("serving-mode", "demo")
     projector.setAttribute("projector-config-json-path", "standalone_projector_config.json")
     projector.setAttribute("data-input", dataStr)
     document.getElementById('socrat-projector').appendChild(projector)
+    const s = document.createElement('script');
+    s.setAttribute('src', "https://cdn.jsdelivr.net/gh/lm233/tensorprojector/standalone.js" );
+    document.getElementById('socrat-projector').appendChild(s);
 }
 
 module.exports = socratProjector
